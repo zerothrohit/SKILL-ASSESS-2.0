a93650e625a3b4cf90d14fdcadd805deb434f2e4
Vaishnavi0805 <sarmalkar.vaishnavi2003@gmail.com>
2024-01-22 14:50:50 +0530

    Added login system

--- skill_assess/home/views.py
+++ skill_assess/home/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
+def register(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        confirmPassword=request.POST['confirmPassword']
+
+        myuser=User.objects.create_user(username,email,password)
+        myuser.save()
+        messages.success(request,"Your skillassess account has been successfully created.")
+        return redirect('login')
+    
+    return render(request,'register.html')
+
+def Login(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=authenticate(username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return render(request,"about.html",{'username':username})
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+            return render(request, "login.html", {'error_message': "Invalid username or password."})
+
+    return render(request, "login.html")
 def about(request):
     return render(request,'about.html')
 
--- skill_assess/skill_assess/settings.py
+++ skill_assess/skill_assess/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -75,13 +76,25 @@ WSGI_APPLICATION = 'skill_assess.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# Replace name, Password, Host with your RDS Database name, user, password, host.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'skillassessdb',
+        'USER': 'skillassess',
+        'PASSWORD': 'skillassess2024',
+        'HOST': 'skillassessdb.cpiky2qmkt1e.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
