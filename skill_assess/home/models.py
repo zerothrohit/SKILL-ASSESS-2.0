92acfadb90df6e055e9ad9a7ea611d973dbce14e
Vaishnavi0805 <sarmalkar.vaishnavi2003@gmail.com>
2024-01-24 18:40:57 +0530

    Obtained data in db

--- skill_assess/home/models.py
+++ skill_assess/home/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Resume(models.Model):
     file_path = models.FileField(upload_to='skill_assess/static/resumes') 
-    round_type = models.CharField(max_length=50)
-    job_title = models.CharField(max_length=100)
+    round_type = models.TextField()
+    job_title = models.TextField()
     job_description = models.TextField()
 
     def __str__(self):
